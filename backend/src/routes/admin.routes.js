623e2ed0dca7a44e26605892e5e85c6fb1d8779a
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-10-31 16:25:49 +0530

    Add JWT verification middleware to admin logout route

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,11 +1,12 @@
 
 import { Router } from 'express';
 import { loginController, logoutController, signupController } from '../controllers/admin.controller';
+import { verifyJWTAdmin } from './../middlewares/admin.middlewares';
 
 const router = Router();
 
 router.route("/signup").post(signupController);
 router.route("/login").post(loginController);
-router.route("/logout").post(logoutController);
+router.route("/logout").post(verifyJWTAdmin, logoutController);
 
 export default router;
